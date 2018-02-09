de0e6fbf68e920084f6910efc15de0a4dc19ae71
RyanSquared <vandor2012@gmail.com>
2018-02-09 00:15:11 -0600

    examples/email-example: Add example for email verifier

---
+++ examples/email-example.py
@@ -0,0 +1,33 @@
+# pylint: disable-all
+
+import spudbucket as sb
+import flask
+
+app = flask.Flask(__name__)
+
+# How to Use:
+#
+# `curl -F 'email=ryan@hashbang.sh'`
+# `curl -F 'email=ryan@notcorrect.sh'`
+# `curl -F example=bad1input localhost:5000`
+# `curl localhost:5000`
+
+
+@app.route("/", methods=["GET", "POST"])
+@sb.set_methods("POST")
+@sb.validator(sb.v.EmailValidator("email", domain="hashbang.sh"))
+@sb.base
+def index(form):
+    if form.is_form_mode():
+        # Method is POST and form fields are valid
+        return repr(form)
+    return 'hi!'
+
+
+@app.errorhandler(sb.e.FormError)
+def handle_form_error(exc):
+    return str(exc), 400
+
+
+if __name__ == "__main__":
+    app.run()
