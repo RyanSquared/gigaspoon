93287121d06747f0c9d5e22408336b605f17c480
RyanSquared <vandor2012@gmail.com>
2019-10-21 09:27:16 -0500

    setup.py: Add gigaspoon.integrations package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='gigaspoon',
     version='0.1.0',
-    packages=['gigaspoon'],
+    packages=['gigaspoon', 'gigaspoon.integrations'],
     extras_require={
         "dev": ["pytest", "pytest-cov"],
         "flask": ["flask"],
