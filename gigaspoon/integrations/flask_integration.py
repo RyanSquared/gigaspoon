dd65f507bd165c57237c6a35a5cc3f4d5ba7bdea
RyanSquared <vandor2012@gmail.com>
2020-04-11 03:22:16 -0500

    Move validate_item outside of flask integration
    New location is the `u.validate_item()` function.

--- gigaspoon/integrations/flask_integration.py
+++ gigaspoon/integrations/flask_integration.py
@@ -146,26 +146,6 @@ def get_form(methods: List[str] = ["POST"]):
     return form
 
 
-def validate_item(validator_list, name, item):
-    """
-    Validate an item across a set of validators. A name should be passed
-    through representing the entire search path of the object, to make
-    debugging client issues easier. For example, a value of "y" in a dict "x"
-    should have a name value of "x.y".
-    """
-    if not isinstance(validator_list, Iterable):
-        validator_list = [validator_list]
-
-    # Iterate through all validators
-    for validator in validator_list:
-        # Check to make sure input is valid
-        opt_value = validator.validate(name, item)
-        if opt_value is not None:
-            item = opt_value
-
-    return item
-
-
 # Prototype decorator for validating incoming requests
 def _validator_prototype(func: Callable, validators, *args, **kwargs):
     for name, validator_list in validators.items():
@@ -193,7 +173,7 @@ def _validator_prototype(func: Callable, validators, *args, **kwargs):
                     item = json[name]
 
                 # Data is valid, can put into our local form
-                form[name] = validate_item(validator_list, name, item)
+                form[name] = u.validate_item(validator_list, name, item)
         else:
             for name, validator_list in validators.items():
                 for validator in validator_list:
--- gigaspoon/u.py
+++ gigaspoon/u.py
@@ -1,2 +1,25 @@
+from collections.abc import Iterable
+
+
 def sanitize(validator_name: str, fields: dict) -> dict:
     return {f"{validator_name}_{k}": v for k, v in fields.items()}
+
+
+def validate_item(validator_list, name, item):
+    """
+    Validate an item across a set of validators. A name should be passed
+    through representing the entire search path of the object, to make
+    debugging client issues easier. For example, a value of "y" in a dict "x"
+    should have a name value of "x.y".
+    """
+    if not isinstance(validator_list, Iterable):
+        validator_list = [validator_list]
+
+    # Iterate through all validators
+    for validator in validator_list:
+        # Check to make sure input is valid
+        opt_value = validator.validate(name, item)
+        if opt_value is not None:
+            item = opt_value
+
+    return item
